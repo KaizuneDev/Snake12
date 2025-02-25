d691a365cb4cacf5020403863e3c55ffa52a0fe6
Kaizune <91921765+KaizuneDev@users.noreply.github.com>
2025-02-25 22:19:59 +0100

    Update gridSize and right button

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const canvas = document.getElementById('gameCanvas');
 const ctx = canvas.getContext('2d');
 
-const gridSize = 20;
+const gridSize = 21;
 const tileCount = canvas.width / gridSize;
 
 let snake = [{ x: 10, y: 10 }];
